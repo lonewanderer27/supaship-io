efda08d67d5769f8f55cdc9f88493f7208b8ac5b
lonewanderer27 <adrianejames27@gmail.com>
2024-01-11 01:34:37 +0800

    add api response type for get single post with comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,12 @@ export interface GetPostsResponse {
   username: string;
 }
 
+export interface GetSinglePostWithCommentResponse {
+  author_name: string;
+  content: string;
+  created_at: string;
+  id: string;
+  path: string;
+  score: number;
+  title: string;
+}
